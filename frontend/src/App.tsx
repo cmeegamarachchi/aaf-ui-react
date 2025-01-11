2b56c76facef6af2cad2080addfc05d2b664c520
Chintana Meegamarachchi <chintana@appsolve.com.au>
2025-01-11 03:35:07 +0000

    Initialize frontend with Vite, React, and TypeScript setup

---
+++ frontend/src/App.tsx
@@ -0,0 +1,18 @@
+import './App.css'
+
+import { Button } from "@/components/ui/button"
+
+function Home() {
+  return (
+    <div>
+      <Button>Click me</Button>
+    </div>
+  )
+}
+
+
+function App() {
+  return <Home />
+}
+
+export default App
